1002e3dbb241dc2c29afaf15876d8e0a7b89d49a
Dennis Nyström <dennis.nystrom@gmail.com>
2017-06-02 22:51:32 +0200

    [video] first video test @ pi

--- video/index.js
+++ video/index.js
@@ -1,15 +1,12 @@
 const settings = require('../_shared/settings');
-
 const io = require('socket.io-client');
+const OmxManager = require('omx-manager');
 
+const manager = new OmxManager();
+manager.create('default.mp4', {'--loop': true});
 
 const socket = io.connect(`http://${settings.server.host}:${settings.server.port}`);
-
 socket.on('connect', () => {});
 
 
-socket.emit('command', {
-    sender: 'video',
-    action: 'hello',
-    data: 'hey everybody!'
-});
+
