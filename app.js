a5db9719c31d20311088e70a74ed236725a0406f
Dennis Nyström <dennis.nystrom@gmail.com>
2017-06-06 00:37:38 +0200

    renamed apps' index files to app name for consistency

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 // start all apps
 require('./server/server.js');
-require('./audio-player/server.js');
-require('./video/index.js');
+require('./audio-player/audio-player.js');
+require('./video/video.js');
